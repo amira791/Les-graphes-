67c5bbf2538d26632d1d2831ac4e61cdaf202577
amira791 <ka_bellali@esi.dz>
2023-12-02 12:59:39 +0100

    AP

--- fonctionAP.js
+++ fonctionAP.js
@@ -1,47 +1,57 @@
+function DFS(u, graph, discovery, low, parent, aps) {
+    let children = 0;
+    discovery[u] = low[u] = ++time;
 
-/* La complexite de DFS : O(V + E) ou V: nb noeuds visites et E: nb arcs visites  */
-function DFS (root, graph, visited = new Set())
-{
-    console.log(root);
-    visited.add(root);
+    const neighbors = graph[u];
 
-    const adjacents = graph[root] || [];
+    for (const v of neighbors) {
+        if (!discovery[v]) {
+            children++;
+            parent[v] = u;
+            DFS(v, graph, discovery, low, parent, aps);
 
-    for (const adjacent of adjacents )
-        {
-            if (!visited.has(adjacent))
-            {
-                DFS(adjacent, graph, visited);
+            low[u] = Math.min(low[u], low[v]);
+
+            if (parent[u] === -1 && children > 1) {
+                aps.add(u);
             }
-        }
 
+            if (parent[u] !== -1 && low[v] >= discovery[u]) {
+                aps.add(u);
+            }
+        } else if (v !== parent[u]) {
+            low[u] = Math.min(low[u], discovery[v]);
+        }
+    }
 }
 
-function find_AP_Torjan (graph)
-{
-    const APs = new Set();
-    const visited = new Set();
+function find_AP_Tarjan(graph) {
+    const aps = new Set();
+    const discovery = {};
+    const low = {};
+    const parent = {};
+    time = 0;
 
-   for (node in graph)
-   {
-    if (!visited.has(node))
-    {
-        DFS (node, graph, visited)
-        const connectedComponent = new Set([...visited]);   
-
-        for (const adjacent of graph[node])
-        {
-            visited.delete(adjacent);
+    for (const node in graph) {
+        if (!discovery[node]) {
+            parent[node] = -1;
+            DFS(node, graph, discovery, low, parent, aps);
         }
     }
 
-    if (connectedComponent.size < Object.keys(graph).length)
-    {
-        APs.add(node);
-    }
-   }
-
-   return Array.from(APs);
-
+    return Array.from(aps);
 }
 
+const sampleGraph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D'],
+    'C': ['A', 'D', 'E'],
+    'D': ['B', 'C'],
+    'E': ['C'],
+};
+
+// Call the function to find articulation points
+const articulationPoints = find_AP_Tarjan(sampleGraph);
+
+// Display the result
+console.log('Articulation Points:', articulationPoints);
